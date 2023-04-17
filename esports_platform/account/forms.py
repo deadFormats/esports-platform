e77d8f3daf60df149b52b92f5d8f7bc85db0bcc7
deadFormats <deadFormats@proton.me>
2023-04-17 11:48:33 +0000

    Update forms.py

--- esports_platform/account/forms.py
+++ esports_platform/account/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-from django.forms import BoundField
-from django.utils.safestring import mark_safe
-from django.templates.loader import get_template
-
-
 
 class LoginForm(forms.Form):
     username = forms.CharField()
